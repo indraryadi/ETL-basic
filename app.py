d75bb4c9488c2e6978cbe539ca63e88e176eabf3
indraryadi <indraaryadikadek@gmail.com>
2022-05-10 05:52:40 -0400

    insert all dim into dwh

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import json
+from unicodedata import category
 import pandas as pd
 import numpy as np
 import sqlalchemy
 
 from connections.mysql import MySQL
+from connections.postgresql import PostgreSql
+
+import Transform
 
 def read_credentials():
     with open('credentials.json','r') as d:
@@ -66,9 +70,44 @@ def insert_raw_data_to_mysql():
     except (Exception) as e:
         print(e)        
     
-    
+    engine.dispose()
 #continue to Transform data
+
+def insert_dim_to_dwh(schema):
+    cfg=read_credentials()['postgresql_warehouse']
+    postgre_auth=PostgreSql(cfg)
+    engine,engine_conn=postgre_auth.conn(conn_type='engine')
+    
+    transform=Transform.Transform()
+    category=transform.dim_category()
+    country=transform.dim_country()
+    channel=transform.dim_channel()
+    video=transform.dim_video()
+    time=transform.dim_time()
+    
+    try:
+        print("Try insert dim category into DWH...")
+        # category.to_sql('dim_category',schema=schema,con=engine,if_exists="replace",index=False)
+        print("Success !!!")
         
+        print("Try insert dim country into DWH...")
+        # country.to_sql('dim_country',schema=schema,con=engine,if_exists="replace",index=False)
+        print("Success !!!")
         
+        print("Try insert dim channel into DWH...")
+        # channel.to_sql('dim_channel',schema=schema,con=engine,if_exists="replace",index=False)
+        print("Success !!!")
+        
+        print("Try insert dim video into DWH...")
+        # video.to_sql('dim_video',schema=schema,con=engine,if_exists="replace",index=False)
+        print("Success !!!")
+        
+        print("Try insert dim time into DWH...")
+        time.to_sql('dim_time',schema=schema,con=engine,if_exists="replace",index=False)
+        print("Success !!!")
+        # print(country)
+    except (Exception) as e:
+        print(e)
 if __name__=='__main__':
-    insert_raw_data_to_mysql()
+    # insert_raw_data_to_mysql()
+    insert_dim_to_dwh(schema='public')  
